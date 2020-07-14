dc223ade9983d077ff1ef4a3f6f98e3079ad2eed
MK404xL <shinydark.sky@gmail.com>
2020-07-14 15:43:46 +0700

    First commit branch components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-6 col-xl-6 header" >
-
-      </div>
-        <div className="col-6 col-xl-6 header " >
-
-      </div>
-        <div class="col-12 col-xl-12 result">
-          <button class="btn btn-success btn-lg button-result">
-                  Result <span class="badge badge-primary"></span>
-          </button>
-      </div>
-    </div>
-
+      
     </div>
   )
 }
